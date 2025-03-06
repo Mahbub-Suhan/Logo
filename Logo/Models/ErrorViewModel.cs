0a38b866c1a3baad3146b683ad7fc28483bcc236
Mahbub-Suhan <immahbubsuhan2000@gmail.com>
2025-03-06 18:37:56 +0600

    Add project files.

---
+++ Logo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Logo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
